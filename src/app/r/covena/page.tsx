4bb0011d9a94ca7e42fc61d142ad6793ca4ea144
anshori <anshoriacc@gmail.com>
2025-08-27 22:54:40 +0700

    add complete text

--- src/app/r/covena/page.tsx
+++ src/app/r/covena/page.tsx
@@ -76,10 +76,10 @@ export default async function CovenaPage() {
       </p>
 
       <p>
-        Logout button also destructive to the current sessions, so it&apos;s better
-        hiding inside a dropdown menu to reduce accidental clicks. I think this
-        patterns is widely used in so many web app nowadays. We can see the
-        improved placement from the example provided below.
+        Logout button also destructive to the current sessions, so it&apos;s
+        better hiding inside a dropdown menu to reduce accidental clicks. I
+        think this patterns is widely used in so many web app nowadays. We can
+        see the improved placement from the example provided below.
       </p>
 
       <SidebarDemo />
@@ -101,9 +101,21 @@ export default async function CovenaPage() {
 
       <AutomationCard />
 
+      <p>
+        We can see that after the improvement I made, the automation cards are
+        now more visually distinct and easier to scan. I also reduced the action
+        buttons, to prioritize edit & start/pause and move the other to the
+        kebab dropdown menu for a cleaner look.
+      </p>
+
       <a href="#conversations" className="mt-16 block">
         <h2 id="conversations">Conversations</h2>
       </a>
+
+      <p>
+        Next parts is conversations page. Just like the previous page, I think
+        hieararchy in this page also need to be improved.
+      </p>
 
       <figure>
         <div>
@@ -118,9 +130,24 @@ export default async function CovenaPage() {
         <figcaption>Conversations Page</figcaption>
       </figure>
 
+      <p>
+        First that I noticed from this page is, the chat list show colored chips
+        (AI Agent, ongoing-qa, etc). I think we need to unify it to reduce
+        noise. Second one is the time semantics on chat list, I think we need to
+        add a hover effect to show the complete time information. This way,
+        users can quickly scan relative times (like "2h ago") but still access
+        the exact timestamp when needed, improving clarity without cluttering
+        the UI.
+      </p>
+
       <a href="#analytics" className="mt-16 block">
         <h2 id="analytics">Analytics</h2>
       </a>
+
+      <p>
+        This page shows 2 big donut charts that show successful payments and
+        conversations.
+      </p>
 
       <figure>
         <div>
@@ -135,131 +162,26 @@ export default async function CovenaPage() {
         <figcaption>Analytics Page</figcaption>
       </figure>
 
-      <LoremIpsum />
+      <p>
+        My take on this page is that the donut charts are effective in conveying
+        the overall trends at a glance, but they could benefit from additional
+        context or detail on hover to provide more insights without overwhelming
+        the user.
+      </p>
+
+      <p>
+        I personally dont like donut charts, I think donut consume more spaces
+        than charts like bar charts, but I dont know the context, with a right
+        context, they could be more effective.
+      </p>
+
+      <p>
+        The colors need to improve because purple and blue are low contrast
+        (even tho I know its Covena's color brand), but I think user's
+        accessibility and readability should take priority. Consider using
+        higher contrast colors or providing alternative color schemes for better
+        visibility, especially for users with visual impairments.
+      </p>
     </article>
   );
 }
-
-const LoremIpsum = () => {
-  return (
-    <section className="space-y-6">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-        fermentum tellus in ornare facilisis. Aliquam molestie molestie mi vel
-        interdum. Duis faucibus sem ut maximus ullamcorper. Aenean vel varius
-        tortor, sed gravida diam. Quisque at velit efficitur, gravida erat vel,
-        vehicula quam. Duis id odio et elit accumsan dictum eget sed lorem.
-        Nulla sollicitudin eros mauris, faucibus egestas risus aliquet quis.
-        Nunc blandit felis nibh, sit amet porta mauris maximus vitae. Nulla
-        vulputate turpis sed euismod sollicitudin. Duis sit amet metus at mauris
-        iaculis cursus ut non urna. Vestibulum efficitur enim nulla, quis cursus
-        ipsum efficitur quis. Aenean eget hendrerit dolor, vitae sollicitudin
-        quam. Ut aliquet libero ipsum, ac laoreet quam lacinia in. Nunc pharetra
-        nulla ac luctus sollicitudin. Phasellus mollis imperdiet ultricies.
-      </p>
-      <p>
-        Sed sed eros at enim malesuada pharetra lacinia et orci. Mauris rutrum
-        commodo velit eu rhoncus. Aliquam felis sem, dapibus ac lacinia at,
-        iaculis vitae libero. Praesent nibh mi, congue id risus eu, rutrum
-        ullamcorper metus. Pellentesque habitant morbi tristique senectus et
-        netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Maecenas vestibulum, purus in lobortis
-        ultrices, ante orci vulputate orci, quis faucibus quam massa ut mauris.
-        Nulla sed aliquam lacus, eget mollis enim. Morbi vulputate, augue sit
-        amet bibendum tincidunt, purus sapien iaculis orci, vel tincidunt arcu
-        odio sit amet risus. Aliquam at nunc vulputate nulla feugiat porta. Nam
-        feugiat neque quis maximus iaculis.
-      </p>
-      <p>
-        Vivamus tincidunt urna ut feugiat hendrerit. Mauris eu lacus vitae est
-        pellentesque euismod. Nam arcu nisi, tempor vel tortor sed, dictum
-        tincidunt est. Proin in eleifend nulla. Proin nec luctus velit.
-        Phasellus dictum dolor id ligula vulputate vestibulum. Nunc accumsan
-        velit nisi, sit amet semper ligula convallis in. Pellentesque sed nisi
-        eros. Vestibulum tempor iaculis leo, a feugiat mauris eleifend a. Nunc
-        pellentesque venenatis gravida. Nulla vitae ullamcorper ligula. Donec
-        venenatis viverra nunc, vel cursus tortor molestie sed.
-      </p>
-      <p>
-        Integer posuere leo vitae nibh lobortis tempus. Donec leo erat, accumsan
-        sit amet mollis rhoncus, mattis nec turpis. Pellentesque id sem a tellus
-        molestie tempus. Vivamus enim dui, eleifend at ligula vitae, tempor
-        sodales purus. Nullam rhoncus ipsum consectetur, vulputate lectus vel,
-        blandit mi. Praesent a lectus consequat, viverra metus eu, facilisis
-        magna. Vivamus ut aliquam felis, placerat facilisis dui. Vivamus in
-        dignissim velit. Pellentesque eu enim nisl. Phasellus a convallis urna.
-      </p>
-      <p>
-        Praesent vulputate sed velit a pharetra. Nulla et diam eget mi varius
-        congue. Curabitur blandit volutpat enim a facilisis. Maecenas et augue
-        vitae nibh euismod auctor sit amet et nulla. Phasellus condimentum
-        scelerisque est in venenatis. Proin sodales efficitur lectus quis
-        facilisis. Mauris odio ex, ornare a eros et, dapibus blandit justo. In
-        vitae augue cursus, mattis dolor nec, varius nisi. Mauris vel ante eu
-        orci molestie porta.
-      </p>
-      <p>
-        In vitae eros rhoncus, fermentum turpis quis, sollicitudin eros.
-        Curabitur sit amet consectetur massa, ac porta mi. Nullam ut blandit
-        est. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Aliquam urna metus, tristique eget sodales porttitor, vulputate eget
-        orci. Vivamus congue vehicula aliquet. Quisque id lacus posuere, sodales
-        leo rhoncus, sodales arcu. Fusce facilisis rhoncus turpis eget molestie.
-        Sed vel mauris sed dolor laoreet dapibus. Maecenas rhoncus bibendum
-        lorem, in pulvinar risus lobortis eget. Curabitur eget metus mauris.
-        Duis rhoncus tellus justo, at sodales turpis suscipit ut.
-      </p>
-      <p>
-        Vivamus nec iaculis nibh, non ullamcorper risus. Mauris at rutrum odio.
-        Pellentesque at tempor ex, ut condimentum nunc. Praesent ligula orci,
-        volutpat ac ligula ut, semper condimentum eros. Praesent tristique non
-        tellus viverra sagittis. Phasellus nibh justo, ultricies et sodales ac,
-        semper quis sem. Sed in sollicitudin sapien, blandit hendrerit erat. In
-        sed metus eget enim iaculis eleifend. Nulla facilisi. Sed euismod nisi
-        accumsan varius mollis. Phasellus mollis dui libero. Mauris id vulputate
-        dolor, in scelerisque leo. Duis dapibus turpis odio, vel tempor libero
-        egestas a. Phasellus neque leo, pretium id sagittis interdum, malesuada
-        eu tortor.
-      </p>
-
-      <p>
-        Phasellus et bibendum nisi, non euismod sem. Mauris venenatis fringilla
-        dapibus. Phasellus bibendum suscipit orci, sit amet efficitur ipsum
-        vulputate sed. Vestibulum ante ipsum primis in faucibus orci luctus et
-        ultrices posuere cubilia curae; Aliquam vitae sapien accumsan, lobortis
-        velit sit amet, ultricies lorem. Mauris ut nisi non dui tempor gravida
-        vel vitae risus. Aenean sodales dui sed dolor imperdiet posuere. Sed
-        tempor vel augue sit amet efficitur. Cras ut urna sit amet nulla
-        fringilla fringilla a at lectus. Nam rhoncus, lectus sollicitudin
-        aliquet finibus, metus purus egestas nisl, nec placerat nunc lorem ut
-        leo. Quisque malesuada turpis turpis, in cursus mauris sodales sed.
-        Phasellus vulputate nulla a cursus fermentum. Integer at erat semper,
-        consequat neque vel, dictum purus. Donec sed mattis ligula.
-      </p>
-      <p>
-        Nunc viverra congue dolor, quis fringilla dolor efficitur ac.
-        Suspendisse sagittis ligula erat, nec feugiat leo varius tristique.
-        Proin laoreet non lacus ultricies ultricies. Etiam vehicula est velit,
-        et posuere urna posuere quis. Aenean ex enim, volutpat id vulputate vel,
-        luctus vitae nisl. Phasellus vel magna eget metus scelerisque
-        scelerisque et nec orci. Curabitur turpis erat, posuere eleifend lectus
-        eget, molestie maximus quam. Mauris consequat magna eu eros sagittis
-        bibendum. Praesent vitae tortor posuere, sodales magna ut, egestas
-        tellus. Maecenas laoreet commodo massa, sit amet tristique orci faucibus
-        a. Curabitur pretium varius elementum. In volutpat ex a ligula porta, a
-        faucibus tortor varius. Fusce mi mi, commodo eget tortor et, dignissim
-        maximus metus.
-      </p>
-      <p>
-        Nam et ipsum eget lorem lobortis efficitur. Cras varius tortor lectus,
-        nec tincidunt justo luctus eget. Mauris bibendum augue et semper tempus.
-        Sed ultricies nibh a consectetur molestie. Pellentesque malesuada augue
-        eget ex congue scelerisque vitae nec mi. Ut a ex lobortis, commodo diam
-        vel, sollicitudin urna. Nunc sodales viverra nibh, id sagittis neque
-        tristique et. Morbi consectetur suscipit ante tristique laoreet.
-        Vestibulum eleifend dui at nisl posuere sagittis. Nam et felis
-        vestibulum, pretium ex vitae, tempus quam.
-      </p>
-    </section>
-  );
-};
--- src/components/covena/automation-card.tsx
+++ src/components/covena/automation-card.tsx
@@ -1,17 +1,27 @@
 import {
   ChevronDownIcon,
+  EyeIcon,
   MoreVerticalIcon,
   PauseIcon,
   PencilIcon,
+  PlayIcon,
+  TrashIcon,
 } from "lucide-react";
 import { Card } from "../ui/card";
 import { Button } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export const AutomationCard = () => {
   return (
     <div className="space-y-2">
-      <Card className="relative isolate aspect-[4/3] w-full overflow-hidden">
-        <div className="flex min-h-full flex-col gap-4 overflow-y-scroll px-6">
+      <Card className="relative isolate aspect-[4/3] w-full overflow-hidden py-0">
+        <div className="flex min-h-full flex-col gap-4 overflow-y-scroll p-6">
           <Card className="bg-background flex w-full flex-col rounded-md border-l-4 border-l-[#4B86E3] p-4 py-4">
             <div className="grid grid-cols-[2rem_1fr] items-center gap-4">
               <ChevronDownIcon />
@@ -34,19 +44,221 @@ export const AutomationCard = () => {
                 </div>
 
                 <div className="flex gap-1">
-                  <Button variant="outline" size="icon" className="size-7">
-                    <PauseIcon className="size-4" />
-                  </Button>
-                  <Button variant="outline" size="icon" className="size-7">
-                    <PencilIcon className="size-4" />
-                  </Button>
-                  <Button variant="outline" size="icon" className="size-7">
-                    <MoreVerticalIcon className="size-4" />
-                  </Button>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="outline" size="icon" className="size-7">
+                        <PauseIcon className="size-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Pause Automation</p>
+                    </TooltipContent>
+                  </Tooltip>
+
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="outline" size="icon" className="size-7">
+                        <PencilIcon className="size-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Edit Automation</p>
+                    </TooltipContent>
+                  </Tooltip>
+
+                  <DropdownMenu>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            variant="outline"
+                            size="icon"
+                            className="size-7">
+                            <MoreVerticalIcon className="size-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>More Options</p>
+                      </TooltipContent>
+                    </Tooltip>
+                    <DropdownMenuContent align="end" className="dark text-xs">
+                      <DropdownMenuItem>
+                        <EyeIcon />
+                        View Automation
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <TrashIcon />
+                        Delete Automation
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
               </div>
             </div>
           </Card>
+
+          <Card className="bg-background flex w-full flex-col rounded-md border-l-4 border-l-[#4B86E3] p-4 py-4">
+            <div className="grid grid-cols-[2rem_1fr] items-center gap-4">
+              <ChevronDownIcon />
+              <div className="flex justify-between gap-4">
+                <div className="flex flex-col gap-1">
+                  <div className="flex items-center gap-1">
+                    <h3 className="text-sm font-semibold">New Automation</h3>
+                    <span className="flex h-5 items-center rounded-full bg-neutral-500/10 px-2 text-xs font-medium text-neutral-500">
+                      Paused
+                    </span>
+                  </div>
+
+                  <p className="text-xs text-neutral-500">
+                    Trigger: Customer Never Engaged (24h).
+                    <br />
+                    Stop on reply: Yes.
+                    <br />
+                    Max runs: 1
+                  </p>
+                </div>
+
+                <div className="flex gap-1">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="outline" size="icon" className="size-7">
+                        <PlayIcon className="size-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Start Automation</p>
+                    </TooltipContent>
+                  </Tooltip>
+
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="outline" size="icon" className="size-7">
+                        <PencilIcon className="size-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Edit Automation</p>
+                    </TooltipContent>
+                  </Tooltip>
+
+                  <DropdownMenu>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            variant="outline"
+                            size="icon"
+                            className="size-7">
+                            <MoreVerticalIcon className="size-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>More Options</p>
+                      </TooltipContent>
+                    </Tooltip>
+                    <DropdownMenuContent align="end" className="dark text-xs">
+                      <DropdownMenuItem>
+                        <EyeIcon />
+                        View Automation
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <TrashIcon />
+                        Delete Automation
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              </div>
+            </div>
+          </Card>
+
+          {[...Array(2)].map((_, i) => (
+            <Card
+              key={i}
+              className="bg-background flex w-full flex-col rounded-md border-l-4 border-l-[#4B86E3] p-4 py-4">
+              <div className="grid grid-cols-[2rem_1fr] items-center gap-4">
+                <ChevronDownIcon />
+                <div className="flex justify-between gap-4">
+                  <div className="flex flex-col gap-1">
+                    <div className="flex items-center gap-1">
+                      <h3 className="text-sm font-semibold">New Automation</h3>
+                      <span className="flex h-5 items-center rounded-full bg-green-500/10 px-2 text-xs font-medium text-green-500">
+                        Active
+                      </span>
+                    </div>
+
+                    <p className="text-xs text-neutral-500">
+                      Trigger: Customer Never Engaged (24h).
+                      <br />
+                      Stop on reply: Yes.
+                      <br />
+                      Max runs: 1
+                    </p>
+                  </div>
+
+                  <div className="flex gap-1">
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button
+                          variant="outline"
+                          size="icon"
+                          className="size-7">
+                          <PauseIcon className="size-4" />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Pause Automation</p>
+                      </TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button
+                          variant="outline"
+                          size="icon"
+                          className="size-7">
+                          <PencilIcon className="size-4" />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Edit Automation</p>
+                      </TooltipContent>
+                    </Tooltip>
+
+                    <DropdownMenu>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <DropdownMenuTrigger asChild>
+                            <Button
+                              variant="outline"
+                              size="icon"
+                              className="size-7">
+                              <MoreVerticalIcon className="size-4" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>More Options</p>
+                        </TooltipContent>
+                      </Tooltip>
+                      <DropdownMenuContent align="end" className="dark text-xs">
+                        <DropdownMenuItem>
+                          <EyeIcon />
+                          View Automation
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          <TrashIcon />
+                          Delete Automation
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                </div>
+              </div>
+            </Card>
+          ))}
         </div>
       </Card>
 
