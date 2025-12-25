db52c02ee64b299ac44de056071fcf455c7147eb
anshori <anshoriacc@gmail.com>
2025-12-26 00:45:02 +0700

    init tanstack start & shadcn

--- src/hooks/api/spotify.ts
+++
@@ -1,9 +0,0 @@
-import { getCurrentlyPlaying } from "@/data/spotify";
-import { useQuery } from "@tanstack/react-query";
-
-export const useGetCurrentlyPlayingQuery = () =>
-  useQuery({
-    queryKey: ["currently-playing"],
-    queryFn: getCurrentlyPlaying,
-    staleTime: 30 * 1000,
-  });
