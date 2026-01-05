9f7cc79144c1782730a1fac0a14fdcc98ffcf29b
anshori <anshori@travelio.com>
2026-01-05 13:36:18 +0700

    update clock

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -29,45 +29,49 @@ export const Clock = ({ className }: Props) => {
   }, [updateTime])
 
   return (
-    <svg viewBox="0 0 100 100" className={cn('size-7', className)}>
-      <circle
-        id="clock-circle"
-        cx="50"
-        cy="50"
-        r="50"
-        fill="currentColor"
-        className={cn(
-          isClient && (
-            isNight
+    <>
+      <svg viewBox="0 0 100 100" className={cn('size-7', className)}>
+        <circle
+          id="clock-circle"
+          cx="50"
+          cy="50"
+          r="50"
+          fill="currentColor"
+          className={cn(
+            isClient && (
+              isNight
               ? 'fill-sky-400 dark:fill-sky-600'
               : 'fill-amber-400 dark:fill-amber-500'
-          ),
-        )}
-      />
-      <circle cx="50" cy="50" r="2" fill="currentColor" />
-      <line
-        id="hour-hand"
-        x1="50"
-        y1="50"
-        x2="50"
-        y2="30"
-        stroke="currentColor"
-        strokeWidth="3"
-        strokeLinecap="round"
-        transform={`rotate(${hourRotation}, 50, 50)`}
-      />
-      <line
-        id="minute-hand"
-        x1="50"
-        y1="50"
-        x2="50"
-        y2="15"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        transform={`rotate(${minuteRotation}, 50, 50)`}
-      />
-    </svg>
+            ),
+          )}
+          />
+        <circle cx="50" cy="50" r="2" fill="currentColor" />
+        <line
+          id="hour-hand"
+          x1="50"
+          y1="50"
+          x2="50"
+          y2="30"
+          stroke="currentColor"
+          strokeWidth="3"
+          strokeLinecap="round"
+          transform={`rotate(${hourRotation}, 50, 50)`}
+          />
+        <line
+          id="minute-hand"
+          x1="50"
+          y1="50"
+          x2="50"
+          y2="15"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          transform={`rotate(${minuteRotation}, 50, 50)`}
+          />
+      </svg>
+
+      <ClockScript />
+    </>
   )
 }
 
