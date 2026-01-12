fe998f6bbf834dc2ed81c4d5b4e44368bd1696de
anshori <anshoriacc@gmail.com>
2026-01-12 13:48:03 +0700

    update og images

--- src/routes/api/og.tsx
+++ src/routes/api/og.tsx
@@ -10,28 +10,27 @@ export const Route = createFileRoute('/api/og')({
         const subtitle = url.searchParams.get('subtitle')
 
         return new ImageResponse(
-          <div tw="flex p-4 bg-white w-full h-full">
+          <div tw="flex p-2 bg-white w-full h-full">
             <div
               style={{
                 background:
-                  'radial-gradient(ellipse 1200px 630px at 10% 10%, #aaa 0%, #000 90%)',
-                backdropFilter: 'blur(4px)',
+                  'radial-gradient(ellipse at 10% 10%, #666 0%, #000 100%)',
               }}
-              tw="relative flex w-full h-full rounded-3xl"
+              tw="relative flex w-full h-full rounded-3xl p-2"
             >
               {title && (
                 <div tw="flex flex-col items-center justify-center text-center w-full">
-                  <span tw="text-white text-6xl font-semibold">{title}</span>
+                  <span tw="text-white text-4xl font-semibold">{title}</span>
                   {subtitle && (
-                    <span tw="text-neutral-300 text-2xl mt-4">{subtitle}</span>
+                    <span tw="text-neutral-300 text-2xl mt-2">{subtitle}</span>
                   )}
                 </div>
               )}
 
               <div tw="absolute bottom-8 right-8 flex items-center">
                 <div tw="flex flex-col text-neutral-300 justify-center items-end">
-                  <span tw="text-white text-5xl">anshori</span>
-                  <span tw="text-4xl">Software Engineer</span>
+                  <span tw="text-white text-3xl">anshori</span>
+                  <span tw="text-xl">Software Engineer</span>
                 </div>
               </div>
 
@@ -41,14 +40,13 @@ export const Route = createFileRoute('/api/og')({
                   backgroundImage:
                     'url("data:image/svg+xml,%3Csvg viewBox=%220 0 400 400%22 xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22noise%22%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%220.5%22 numOctaves=%223%22 result=%22noise%22 /%3E%3CfeColorMatrix in=%22noise%22 type=%22saturate%22 values=%220%22 /%3E%3C/filter%3E%3Crect width=%22400%22 height=%22400%22 fill=%22%23000%22 filter=%22url(%23noise)%22 opacity=%220.15%22 /%3E%3C/svg%3E")',
                   backgroundSize: '600px 600px',
-                  pointerEvents: 'none',
                 }}
               />
             </div>
           </div>,
           {
-            width: 1200,
-            height: 630,
+            width: 800,
+            height: 420,
           },
         )
       },
