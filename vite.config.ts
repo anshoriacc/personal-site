d9b7c0d50556bd1b738848a72d6c4beafe595ad0
anshori <anshoriacc@gmail.com>
2026-07-18 10:56:07 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,14 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
+  resolve: { tsconfigPaths: true },
   plugins: [
     devtools({ eventBusConfig: { port: 42042 } }),
     nitro(),
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
